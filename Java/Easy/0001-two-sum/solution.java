8187f78b7f46744a80de2ef7f712125a39acfd24
sriram108-coder <kasukurthibhargav8@gmail.com>
2026-08-11 16:34:41 +0530

    feat(leetcode): solve #0001 Two Sum (Easy) [Java]

--- Java/Easy/0001-two-sum/solution.java
+++ Java/Easy/0001-two-sum/solution.java
@@ -1,15 +1,27 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int[2];
-        for(int i =0; i< nums.length; i++){
-            for(int j =i+1; j<nums.length; j++){
-                if(nums[i] + nums[j] == target) {
-                arr[0] = i;
-                arr[1] = j;
-                break;}
-            }
-            
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int num: nums){
+            arr.add(num);
+           
         }
-        return arr;
+        // int i = arr[0];
+        // int end = arr.size()-1;
+        // while(i<end){
+        //     if(arr[i]+arr[end] == target){
+        //         System.out.println("["+i+","+end+"]");
+        //     }
+        //     i++;
+        //     end--;
+        // }
+
+
+        for(int i =0; i<arr.size();i++){
+            for(int j = i+1; j<arr.size();j++){
+                if(arr.get(i) +arr.get(j) == target) return new int[]{i,j};
+            }
+        }
+        return new int[]{};
+
     }
 }
